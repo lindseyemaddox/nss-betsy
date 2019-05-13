bfd09c13dcbf1512a05c44aa9abd868db2ecd812
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-05-13 10:03:36 -0500

    pushing for inspection of TypeError

--- src/scripts/review-data.js
+++ src/scripts/review-data.js
@@ -4,12 +4,13 @@
     Purpose: fetch reviews from database
 */
 
-const baseURL = "http://localhost:8088/products";
+const baseURL = "localhost:8088/products";
 
 const API = {
   getReviews: function () {
-    return fetch(baseURL).then(results => results.json())
+    return fetch(baseURL)
+      .then(results => results.json())
   }
 }
 
-export default API;
+export default API
